93cff632f8138ee651d590c16575571eb05bf807
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-03 23:15:02 +0300

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -10,19 +10,19 @@ export default class Command extends BaseCommand {
             description: "Displays the Moderators' contact info",
             category: 'general',
             usage: `${client.config.prefix}mods`,
-            aliases: ['moderators', 'mod', 'owner']
+            aliases: ['moderators', 'mod', 'owner', 'dev', 'creator']
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
+        let text = '💎 *My Owner and Developer* 💎\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n🌟 *Username: ${
+                (text += `#${index + 1}\n 🎇 *Username: ${
                     user.notify || user.vname || user.name || 'null'
-                }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+                }*\n 👉🏿 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
         text += `\n🌟 Chitoge 🌟 `
         return void M.reply(text)
