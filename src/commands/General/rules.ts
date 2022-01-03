1b470fed02a2739fb93265f5246928f53f03ef30
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-01-04 00:38:02 +0300

    Update rules.ts

--- src/commands/General/rules.ts
+++ src/commands/General/rules.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
         MessageType.image,
                     undefined,
                     undefined,
-                    `_*----🎀[Rule]🎀----*_\n\n❌*DONT ASK FOR DIRTY CONTENT FROM THE BOT*🚫\n*>>>* Use !support to get the Official group link in your dm\n*--->*  If you want to chat with DREADED you can use *!bot/chat (your text)* both are different ai chat bots\n*--->* If you want to add DREADED BOT in your group the contact the owner by *!owner/!mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue or data traffic as well. \n*--->* Dont Dm the bot with commands\n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BANNED SOON* 🚫  `,
+                    `_*----🎀[Rule]🎀----*_\n\n❌*BOT MUST BE AN ADMIN OF THE GROUPS ADDED, OR ELSE IT WILL LEAVE*🚫\n*>>>* Use !support to get the Official group link in your dm\n*--->*  If you want to chat with DREADED you can use *!bot/chat (your text)* both are different ai chat bots\n*--->* If you want to add DREADED BOT in your group the contact the owner by *!owner/!mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue or data traffic as well. \n*--->* Dont Dm the bot with commands\n\n*BOT WILL LEAVE INACTIVE AND VIRUS PRONE GROUPS* 🚫  `,
                     undefined
                 ).catch((reason: any) =>
             M.reply(`✖ An error occurred. Please try again later.`))
