d34d3bcb9e5ccac3dfe17ad8ad55be4461212f3f
vantech254 <91389091+vantech254@users.noreply.github.com>
2022-01-13 20:06:27 +0300

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,14 +17,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '💎 *My Owner and Developer* [Fortunne] 💎\n\n'
+        let text = '💎 *My Owner and Developer* [VANTECH⚡] 💎\n\n'
         filteredMap.forEach(
             (user, index) =>
                 (text += `#${index + 1}\n 🎇 *Username: ${
                     user.notify || user.vname || user.name || 'null'
                 }*\n 👉🏿 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n🌟 DREADED 🌟 `
+        text += `\nVANTECH⚡ `
         return void M.reply(text)
     }
 }
