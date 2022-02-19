384969c6c4d5af6368872d02c8358debdc1b064e
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-02-19 15:34:02 +0300

    Update bp.ts

--- src/commands/Utils/bp.ts
+++ src/commands/Utils/bp.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			command: "bp",
 			description: `Get text image`,
 			aliases: ["bp"],
-			category: "creation",
+			category: "utils",
 			usage: `${client.config.prefix}bp`,
 			baseXp: 50,
 		});
@@ -36,7 +36,7 @@ export default class Command extends BaseCommand {
 					MessageType.image,
 					undefined,
 					undefined,
-					`🌟 Here you go.\n`,
+					`💎 By Dreaded Botto.\n`,
 					undefined
 				).catch((e) => {
 					console.log(
