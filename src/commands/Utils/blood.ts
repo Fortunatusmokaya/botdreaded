5b9188e11cd2e58ecbdc1b81e7b8b6385738c697
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-02-19 15:33:04 +0300

    Update blood.ts

--- src/commands/Utils/blood.ts
+++ src/commands/Utils/blood.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			command: "blood",
 			description: `Get text image`,
 			aliases: ["blood"],
-			category: "creation",
+			category: "utils",
 			usage: `${client.config.prefix}blood`,
 			baseXp: 50,
 		});
@@ -36,7 +36,7 @@ export default class Command extends BaseCommand {
 					MessageType.image,
 					undefined,
 					undefined,
-					`🌟 Here you go.\n`,
+					`💎 By Dreaded Botto.\n`,
 					undefined
 				).catch((e) => {
 					console.log(
