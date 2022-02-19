2d55369beb2d37e78c71ac0a6e5781ecc2415a0a
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-02-19 15:36:58 +0300

    Update pencil.ts

--- src/commands/Utils/pencil.ts
+++ src/commands/Utils/pencil.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			command: "pencil",
 			description: `Get text image`,
 			aliases: ["pencil"],
-			category: "creation",
+			category: "utils",
 			usage: `${client.config.prefix} pencil`,
 			baseXp: 50,
 		});
@@ -36,7 +36,7 @@ export default class Command extends BaseCommand {
 					MessageType.image,
 					undefined,
 					undefined,
-					`🌟 Here you go.\n`,
+					`💎 By Dreaded Botto.\n`,
 					undefined
 				).catch((e) => {
 					console.log(
