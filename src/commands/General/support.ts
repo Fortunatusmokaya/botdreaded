9e3698b9740cfed11e6f55909861aa7ee7614a45
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-02-19 12:58:31 +0300

    Create support.ts

---
+++ src/commands/General/support.ts
@@ -0,0 +1,29 @@
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "support",
+      aliases: ["support"],
+      description: "Gets the support group links",
+      category: "general",
+      usage: `${client.config.prefix}Support`,
+      baseXp: 10,
+    });
+  }
+
+  run = async (M: ISimplifiedMessage): Promise<void> => {
+    await this.client.sendMessage(
+      M.sender.jid,
+      ` _*💎   WELL, IF THAT WAS THE CASE THEN  💎*_\n _*DREADED SUPPORT GROUP*_: Pending...`,
+
+      MessageType.text
+    );
+
+    return void M.reply("Sent you a personal message regarding this.");
+  };
+}
